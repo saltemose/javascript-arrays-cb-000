ac8e4ffefd356f2169f7ad562aac9851275fdaae
Sean Altemose <sean@seanaltemose.com>
2019-04-22 14:42:53 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -1,1 +1,10 @@
 var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"]
+
+function addElementToBeginningOfArray() {
+
+
+}
+
+function destructivelyAddElementToBeginningOfArray() {
+
+}
