683a22be4254599f2c76d975b642f19556dbea8a
Sean Altemose <sean@seanaltemose.com>
2019-04-22 14:59:48 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -28,6 +28,5 @@ function destructivelyRemoveElementFromBeginningOfArray(array){
 }
 
 function removeElementFromBeginningOfArray(array){
-  array.slice(0)
-  return array
+  return array.slice(0)
 }
